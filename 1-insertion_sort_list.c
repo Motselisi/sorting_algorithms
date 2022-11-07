5f5e63aa9895cea4c3bc4ebb4dc002b28941dd09
Motselisi <motselisileketa@gmail.com>
2022-11-07 11:23:56 -0800

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,28 +6,28 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *temp, *pn, *nxt;
+  listint_t *temp, *pn, *nxt;
 
-if (list && *list && (*list)->next)
-{
-temp = (*list)->next;
-pn = temp->prev;
-nxt = temp->next;
+  if (list && *list && (*list)->next)
+    {
+      temp = (*list)->next;
+      pn = temp->prev;
+      nxt = temp->next;
 
-hile (temp)
-{
-if (temp->n < pn->n)
-{
-swap_list(pn, temp, nxt, list);
-}
-temp = nxt;
-if (temp)
-{
-nxt = temp->next;
-pn = temp->prev;
-}
-}
-}
+      while (temp)
+	{
+	  if (temp->n < pn->n)
+	    {
+	      swap_list(pn, temp, nxt, list);
+	    }
+	  temp = nxt;
+	  if (temp)
+	    {
+	      nxt = temp->next;
+	      pn = temp->prev;
+	    }
+	}
+    }
 }
 
 /**
@@ -40,20 +40,20 @@ pn = temp->prev;
 void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h)
 {
 
-while (pn && temp->n < pn->n)
-{
-if (nxt)
-nxt->prev = pn;
-temp->next = pn;
-temp->prev = pn->prev;
-if (pn->prev)
-(pn->prev)->next = temp;
-pn->prev = temp;
-pn->next = nxt;
-nxt = pn;
-pn = temp->prev;
-if ((*h)->prev)
-*h = (*h)->prev;
-print_list(*h);
-}
+  while (pn && temp->n < pn->n)
+    {
+      if (nxt)
+	nxt->prev = pn;
+      temp->next = pn;
+      temp->prev = pn->prev;
+      if (pn->prev)
+	(pn->prev)->next = temp;
+      pn->prev = temp;
+      pn->next = nxt;
+      nxt = pn;
+      pn = temp->prev;
+      if ((*h)->prev)
+	*h = (*h)->prev;
+      print_list(*h);
+    }
 }
